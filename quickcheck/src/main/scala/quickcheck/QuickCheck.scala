10b6a525184553f163a7813c7e71a721050c9aef
Gagandeep Kalra <gagandeepkalra1994@gmail.com>
2018-06-22 23:27:13 +0530

    Scala Program Design: Week 3 initial commit

---
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,16 @@
+package quickcheck
+
+import org.scalacheck.Prop._
+import org.scalacheck._
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+  lazy val genHeap: Gen[H] = ???
+  implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
+
+  property("gen1") = forAll { (h: H) =>
+    val m = if (isEmpty(h)) 0 else findMin(h)
+    findMin(insert(m, h)) == m
+  }
+
+}
